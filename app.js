fd7d079bb257d178fc3412918cdae468f8d9d6fe
javiermrzfever <javier.marzan@feverup.com>
2025-02-24 23:49:25 +0100

    Debug download buttons

---
+++ app.js
@@ -0,0 +1,42 @@
+function detectDevice() {
+  // Use modern userAgentData API if available
+  console.log(1)
+  if (navigator.userAgentData) {
+    console.log(2)
+    const platform = navigator.userAgentData.platform.toLowerCase();
+    console.log(platform)
+    if (platform.includes("android")) return "Android";
+    if (platform.includes("ios") || platform.includes("iphone") || platform.includes("ipad")) return "iOS";
+    return "Computer";
+  }
+
+  // Fallback for older browsers
+  const userAgent = navigator.userAgent.toLowerCase();
+  console.log({userAgent})
+  if (/android/.test(userAgent)) return "Android";
+
+  // Modern iPads report themselves as MacOS, so check screen size
+  if (/ipad|iphone|ipod/.test(userAgent) ||
+    (navigator.platform === "MacIntel" && navigator.maxTouchPoints > 1)
+  ) {
+    return "iOS";
+  }
+
+  return "Computer";
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const deviceType = detectDevice();
+  // const deviceType = 'Android';
+  const androidBtn = document.getElementsByClassName("android-button")[0];
+  const iosBtn = document.getElementsByClassName("ios-button")[0];
+  const pcButtons = document.getElementsByClassName("pc-button");
+
+  if (deviceType === "Android") {
+    androidBtn.classList.remove("hidden");
+  } else if (deviceType === "iOS") {
+    iosBtn.classList.remove("hidden");
+  } else {
+    Array.from(pcButtons).forEach((button) => button.classList.remove("hidden"));
+  }
+});
